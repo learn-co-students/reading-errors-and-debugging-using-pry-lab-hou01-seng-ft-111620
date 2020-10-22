d474ff33a110b89e6bfac96bf2a076ec986b405a
Brian Li <brian.li96@yahoo.com>
2020-10-22 05:00:10 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,8 +2,11 @@
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    10.times do
+      string.insert(0, 's')
+    end
   else
-    string
+     string
   end
+  string
 end
